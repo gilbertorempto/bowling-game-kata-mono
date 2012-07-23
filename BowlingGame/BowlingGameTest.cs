da38191e8beb1c62043448197484daaf221cf84d
Gilberto Rempto <gilbertorempto@github>
2012-07-22 21:42:45 -0300

    Second Test

--- BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGameTest.cs
@@ -6,13 +6,32 @@ namespace BowlingGame
 	[TestFixture()]
 	public class BowlingGameTest
 	{
+		private Game g;
+
+		private void RollMany (int n, int pins)
+		{
+			for (int i = 0; i < n; i++)
+				g.Roll(pins);
+		}
+
+		[SetUp]
+		protected void SetUp ()
+		{
+			g = new Game();
+		}
+
 		[Test()]
 		public void TestGutterGame ()
 		{
-			Game g = new Game();
-			for (int i = 0; i < 20; i++)
-				g.Roll(0);
+			RollMany(20, 0);
 			Assert.AreEqual(0, g.Score);
+		}
+
+		[Test()]
+		public void TestAllOnes ()
+		{
+			RollMany(20, 1);
+			Assert.AreEqual(20, g.Score);
 		}
 	}
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -4,13 +4,16 @@ namespace BowlingGame
 {
 	public class Game
 	{
+		private int score = 0;
+
 		public void Roll (int pins)
 		{
+			score += pins;
 		}
 
 		public int Score {
 			get {
-				return 0;
+				return score;
 			}
 		}
 	}
